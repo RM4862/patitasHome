cd3017d33eed02ed879c4fd845dd03a7dfe843f6
laloaulis <105012448+laloaulis@users.noreply.github.com>
2025-05-13 23:47:06 -0600

    Update mascota.service.ts

--- src/services/mascota.service.ts
+++ src/services/mascota.service.ts
@@ -11,9 +11,17 @@ export class MascotaService {
 
   constructor(private http: HttpClient) { }
 
+  // Método para publicar una mascota encontrada
+  publicarMascotaEncontrada(mascota: any, token: string): Observable<any> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post(this.apiUrl, mascota, { headers });
+
+  }
+
+  // Método para publicar una mascota perdida
   publicarMascotaPerdida(mascota: any, token: string): Observable<any> {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.http.post(this.apiUrl, mascota, { headers });
+
   }
 }
-
